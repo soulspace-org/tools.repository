fc48d94f8c75908274ae2f7be5f7f4fc697bfea3
Ludger Solbach <soulman@soulspace.org>
2023-06-26 09:26:55 +0200

    updated rich comment for purl parsing

--- src/org/soulspace/tools/repo.clj
+++ src/org/soulspace/tools/repo.clj
@@ -266,11 +266,9 @@
 
 
 (comment
-  (sstr/substring 4 "pkg:maven/clj.base@0.8.3")
   (str/split "maven/clj.base@0.8.3?k=v&l=w#/foo" #"(\?|#)")
   (str/split "maven/clj.base@0.8.3?k=v&l=w" #"(\?|#)")
   (str/split "maven/clj.base@0.8.3#/foo" #"(\?|#)")
-  (str/split (sstr/substring 4 "pkg:maven/clj.base@0.8.3?k=v&l=w#/foo") #"(\?|#)")
 
   (generate {:type "maven" :version "0.8.3"})
   (generate {:type "maven" :name "clj.base" :version "0.8.3"})
@@ -285,30 +283,31 @@
   (parse "pkg:maven/clj.base@0.8.3?k=v&l=w#/foo")
   (parse "pkg:maven/org.apache.xmlgraphics/batik-anim@1.9.1?repository_url=repo.spring.io%2Frelease")
 
-  "pkg:bitbucket/birkenfeld/pygments-main@244fd47e07d1014f0aed9c"
+  (parse "pkg:bitbucket/birkenfeld/pygments-main@244fd47e07d1014f0aed9c")
 
-  "pkg:deb/debian/curl@7.50.3-1?arch=i386&distro=jessie"
+  (parse "pkg:deb/debian/curl@7.50.3-1?arch=i386&distro=jessie")
 
-  "pkg:docker/cassandra@sha256:244fd47e07d1004f0aed9c"
-  "pkg:docker/customer/dockerimage@sha256:244fd47e07d1004f0aed9c?repository_url=gcr.io"
+  (parse "pkg:docker/cassandra@sha256:244fd47e07d1004f0aed9c")
+  (parse "pkg:docker/customer/dockerimage@sha256:244fd47e07d1004f0aed9c?repository_url=gcr.io")
 
-  "pkg:gem/jruby-launcher@1.1.2?platform=java"
-  "pkg:gem/ruby-advisory-db-check@0.12.4"
+  (parse "pkg:gem/jruby-launcher@1.1.2?platform=java")
+  (parse "pkg:gem/ruby-advisory-db-check@0.12.4")
 
-  "pkg:github/package-url/purl-spec@244fd47e07d1004f0aed9c"
+  (parse "pkg:github/package-url/purl-spec@244fd47e07d1004f0aed9c")
 
-  "pkg:golang/google.golang.org/genproto#googleapis/api/annotations"
+  (parse "pkg:golang/google.golang.org/genproto#googleapis/api/annotations")
 
-  "pkg:maven/org.apache.xmlgraphics/batik-anim@1.9.1?packaging=sources"
-  "pkg:maven/org.apache.xmlgraphics/batik-anim@1.9.1?repository_url=repo.spring.io%2Frelease"
+  (parse "pkg:maven/org.apache.xmlgraphics/batik-anim@1.9.1?packaging=sources")
+  (parse "pkg:maven/org.apache.xmlgraphics/batik-anim@1.9.1?repository_url=repo.spring.io%2Frelease")
 
-  "pkg:npm/%40angular/animation@12.3.1"
-  "pkg:npm/foobar@12.3.1"
+  (parse "pkg:npm/%40angular/animation@12.3.1")
+  (parse "pkg:npm/foobar@12.3.1")
 
-  "pkg:nuget/EnterpriseLibrary.Common@6.0.1304"
+  (parse "pkg:nuget/EnterpriseLibrary.Common@6.0.1304")
 
-  "pkg:pypi/django@1.11.1"
+  (parse "pkg:pypi/django@1.11.1")
 
-  "pkg:rpm/fedora/curl@7.50.3-1.fc25?arch=i386&distro=fedora-25"
-  "pkg:rpm/opensuse/curl@7.56.1-1.1.?arch=i386&distro=opensuse-tumbleweed")
+  (parse "pkg:rpm/fedora/curl@7.50.3-1.fc25?arch=i386&distro=fedora-25")
+  (parse "pkg:rpm/opensuse/curl@7.56.1-1.1.?arch=i386&distro=opensuse-tumbleweed")
+  )
 
