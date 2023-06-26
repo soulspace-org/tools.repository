e4e60fba32c056ea70ae89ab8e9859b7ab3527f1
Ludger Solbach <soulman@soulspace.org>
2023-06-26 15:31:21 +0200

    released version 0.3.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.soulspace.clj/tools.repository "0.3.7-SNAPSHOT"
+(defproject org.soulspace.clj/tools.repository "0.3.7"
   :description "The tools.repository component provides tools to handle (maven) repositories and artifacts."
   :url "https://github.com/lsolbach/CljDevTools"
   :license {:name "Eclipse Public License"
